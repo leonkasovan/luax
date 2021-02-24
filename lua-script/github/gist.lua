f885b286fef2fe05d87e0584c115115288b66402
Dhani Novan <dhani.novan@gmail.com>
2021-02-25 05:58:22 +0700

    modified:   lua-script/github/gist.lua

--- lua-script/github/gist.lua
+++ lua-script/github/gist.lua
@@ -6,7 +6,7 @@ local DEBUG = true	-- write all log to a file (LOG_FILE)
 --local DEBUG = false	-- write all log to console output
 local LOG_FILE = "github.log"
 local GITHUB_USER = "dhaninovan"
-local GITHUB_TOKEN = "9e39003a512d18de035c6a8c03c37217b95fe2bc"
+local GITHUB_TOKEN = "fdd416f6480d9c26fb64292ee950535b5aa82878"
 local MAXTIMEOUT = 30
 
 local function escape_str(s)
