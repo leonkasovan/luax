4c325b8d7ec7027f8ad8d5c11ad60c5677e12095
dhaninovan <55605773+dhaninovan@users.noreply.github.com>
2021-04-11 20:44:06 +0700

    Update hourly_action.lua
    Load stripped invalid  html content

--- lua-script/hourly_action.lua
+++ lua-script/hourly_action.lua
@@ -3,24 +3,37 @@
 -- Install in crontab : 0 * * * * cd /home/pi/shared/luax/lua-script && lua hourly_action.lua
 
 local LOG_FILE = "multi_host_downloader/multi_host_downloader.log"
-local LOG_FILE2 = "multi_host_downloader/youtube-dl.log"
 
-function myload_file(filename)
-    local fi, content
- 
-    fi = io.open(filename, "r")
-    if fi == nil then
-        return "nil"
-    end
-    content = fi:read("*a")
-    fi:close()
-    return content
+function load_file(filename)
+	local fi, content
+
+	fi = io.open(filename, "r")
+	if fi == nil then
+		print("[error] Load file "..filename)
+		return ""
+	end
+	content = fi:read("*a")
+	fi:close()
+	return filename..':\n'..content..'\n\n'
+end
+
+function strip_html(filename)
+	local fi, content
+
+	fi = io.open(filename, "r")
+	if fi == nil then
+		print("[error] Load file "..filename)
+		return ""
+	end
+	content = fi:read("*a")
+	fi:close()
+	return filename..':\n'..content:gsub("<style.->.-</style>",""):gsub("<script.->.-</script>",""):gsub("<!%-%-.-%-%->",""):gsub("<.->",""):gsub("%s+"," ")..'\n\n'
 end
 
 if os.date("%H") == "05" or os.date("%H") == "22" or os.date("%H") == "17" then
 	os.remove('file.tmp')
-	os.remove(LOG_FILE2)
 end
+
 if os.date("%d")%2 == 0 then
 	if os.date("%H") == "22" then os.remove('multi_host_downloader.log') end
 end
@@ -33,7 +46,14 @@ else
 	os.execute("ls -lt multi_host_downloader > files.txt")
 end
 
-content = string.format("Time:%s\n %s\n\nLOG LUA Downloader:\n%s\n\nLOG Youtube-DL:\n%s", os.date(), myload_file('files.txt'), myload_file(LOG_FILE), myload_file(LOG_FILE2))
---print(content)
-res = gist.update('e1ea2560db98933916e42a1c47bdeec2', 'multi_host_downloader.log', content)
-print(res)
+tcontent = {}
+table.insert(tcontent, string.format("Time:%s\n", os.date()))
+table.insert(tcontent, myload_file('files.txt'))
+table.insert(tcontent, myload_file('multi_host_downloader/multi_host_downloader.log'))
+table.insert(tcontent, myload_file('multi_host_downloader/lua_error.log'))
+table.insert(tcontent, myload_file('multi_host_downloader/bash_error.log'))
+table.insert(tcontent, strip_html('multi_host_downloader/gdrive_invalid_content.htm'))
+table.insert(tcontent, strip_html('multi_host_downloader/videobin_invalid_content.htm'))
+table.insert(tcontent, strip_html('multi_host_downloader/filedot_invalid_content.htm'))
+table.insert(tcontent, strip_html('multi_host_downloader/letsupload_invalid_content.htm'))
+res = gist.update('e1ea2560db98933916e42a1c47bdeec2', 'multi_host_downloader.log', table.concat(tcontent))
