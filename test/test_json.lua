92de3d9c681983c6e75f360b08367d4b04436e26
RYZEN <RYZEN@PC-RYZEN>
2021-06-19 15:57:17 +0700

    modified:   test/test_json.lua 	test/test_table.lua

--- test/test_json.lua
+++ test/test_json.lua
@@ -1,9 +1,17 @@
 -- Lua 5.1.5.XL Test Script for cjson Library
 -- Additional path that may be required
 
+json_text = '{"success":true,"has_posts":true,"new_posts":"ini isinya"}'
+value= json.decode(json_text)
+print(value.success)
+print(value.has_posts)
+print(value.new_posts)
+for i,v in pairs(value) do
+	print(i,v)
+end
+
 json_text = '[ true, { "foo": "bar" } ]'
 value= json.decode(json_text)
-
 for i,v in pairs(value) do
 	print(i,v)
 end
