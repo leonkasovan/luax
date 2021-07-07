2697ff15cd4503ee55ff9bf1d1d0ffffe3ff5e9c
dhaninovan <55605773+dhaninovan@users.noreply.github.com>
2021-07-07 08:12:49 +0700

    Update shell_bot.lua

--- lua-script/shell_bot.lua
+++ lua-script/shell_bot.lua
@@ -4,6 +4,10 @@
 local rc, headers, content, resp, fi, fo
 local local_last_update_id
 
+local TOKEN1 = '1082609389'
+local TOKEN2 = 'AAFJ4OdO7KvWFG24zkzsX6povpOZnv1kU0M'
+local API_TELEGRAM_BOT = 'https://api.telegram.org/bot'..TOKEN1..':'..TOKEN2..'/'
+
 function shell_run(cmd)
 	local fi, res
 	
@@ -17,7 +21,7 @@ function shell_run(cmd)
 	return res
 end
 
-rc, headers, content = http.request('https://api.telegram.org/bot1082609389:AAE8TJPP_57Nqg-boKPOTuLz-U-vYg02CoY/getMe')
+rc, headers, content = http.request(API_TELEGRAM_BOT..'getMe')
 resp = json.decode(content)
 if not resp.ok or resp == nil then
 	print("Invalid response")
@@ -35,18 +39,20 @@ else
 end
 print(os.date()..' Loading last_update_id = '..local_last_update_id..' [done]')
 
-rc, headers, content = http.request('https://api.telegram.org/bot1082609389:AAE8TJPP_57Nqg-boKPOTuLz-U-vYg02CoY/getUpdates')
+rc, headers, content = http.request(API_TELEGRAM_BOT..'getUpdates?offset='..(local_last_update_id+1))
 resp = json.decode(content)
 for i,v in pairs(resp.result) do
-	if v.update_id > local_last_update_id then
-		rc, headers, content = http.request('https://api.telegram.org/bot1082609389:AAE8TJPP_57Nqg-boKPOTuLz-U-vYg02CoY/sendMessage?chat_id='..v.message.chat.id..'&text='..http.escape(shell_run(v.message.text)))
-		print(string.format('%s New text message from %s_%s: %s', os.date(), v.message.from.first_name, v.message.from.last_name, v.message.text))
-		local_last_update_id = v.update_id
-	end
+	print(string.format('%s New text message from %s_%s: %s', os.date(), v.message.from.first_name, v.message.from.last_name, v.message.text))
+	 rc, headers, content = http.request(API_TELEGRAM_BOT..'sendMessage?chat_id='..v.message.chat.id..'&text='..http.escape(shell_run(v.message.text)))
+	-- rc, headers, content = http.request(API_TELEGRAM_BOT..'sendPhoto?chat_id='..v.message.chat.id..'&photo=https://www.windowscentral.com/sites/wpcentral.com/files/styles/larger/public/field/image/2020/10/netstat-cmd-windows-10.jpg')
+	local_last_update_id = v.update_id
 end
-
-fo = io.open("last_update.json", "w+")
-fo:write(string.format('{"last_update_id":%d}', local_last_update_id))
-fo:close()
-print(os.date()..' Writing last_update_id '..local_last_update_id..' [done]')
-
+if #resp.result > 0 then
+	fo = io.open("last_update.json", "w+")
+	fo:write(string.format('{"last_update_id":%d}', local_last_update_id))
+	fo:close()
+	print(os.date()..' Processing '..#resp.result..' message(s)')
+	print(os.date()..' Writing last_update_id '..local_last_update_id..' [done]')
+else
+	print(os.date()..' Nothing new')
+end
