dca6ed391d1be565a14b3cd2dd08a48424359ae6
RYZEN <RYZEN@PC-RYZEN>
2021-10-03 05:41:35 +0700

    modified:   lua-script/vipr/vipr.lua

--- lua-script/vipr/vipr.lua
+++ lua-script/vipr/vipr.lua
@@ -31,18 +31,18 @@ function download_vipr(url, callback_function_write_log, callback_function_on_su
 	end
 	
 	if #content == 0 then
-		write_log("[error][vipr] Empty response. Check response header")
+		write_log("[error][vipr] Empty response. Check response header vipr_invalid_header.txt")
 		save_file(headers,"vipr_invalid_header.txt")
-		update_gist('2ff7b7c90cd7f219043bd450b5c1b05e', 'invalid.htm', headers, 'Header of Empty response')
+		update_gist('2ff7b7c90cd7f219043bd450b5c1b05e', 'vipr_invalid_header.txt', headers, 'Header of Empty response')
 		return nil
 	end
-	
+	write_log("[info][vipr] Content Length = "..tostring(#content))
 	-- Do processing in here
 	media_url = string.match(content, '&nbsp;&nbsp;.<a href="(.-)" download class')
 	if media_url == nil then
 		write_log("[error][vipr] Can't find media url")
 		save_file(content,"vipr_invalid_content.htm")
-		update_gist('2ff7b7c90cd7f219043bd450b5c1b05e', 'invalid.htm', content, 'Fail match: &nbsp;&nbsp;.<a href="(.-)" download class')
+		update_gist('2ff7b7c90cd7f219043bd450b5c1b05e', 'invalid.htm', headers..content, 'Fail match: &nbsp;&nbsp;.<a href="(.-)" download class')
 		return nil
 	end
 	
