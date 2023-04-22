7e44d41c6d4c86d408b10ff3780bb2b7fab4e856
Leon Kasovan <leonkasovan@gmail.com>
2023-04-22 21:15:41 +0700

    modified:   lua-script/archive/archive.lua 	new file:   manual/lua-apiref.pdf 	new file:   manual/lua_capi.pdf 	new file:   manual/luarefv51.pdf

--- lua-script/archive/archive.lua
+++ lua-script/archive/archive.lua
@@ -1,6 +1,10 @@
+-- Generate DB, search from DB, source from archive.org
 -- Source HTM : 
 -- https://archive.org/download/nes-roms
 -- https://archive.org/download/nes-romset-ultra-us
+-- 19:37 22 April 2023, Dhani Novan, Jakarta, Cempaka Putih
+
+dofile('../strict.lua')
 
 function load_file(filename)
 	local fi, content
@@ -15,14 +19,17 @@ function load_file(filename)
 	return content
 end
 
+-- user=aitus95
+-- success, function return table
+-- fail, function return nil
 function archive_user_uploads(user)
 	local res = {}
 	local done = false
-	local page = 1, user_url
+	local page = 1
+	local user_url
 	
 	user_url = 'https://archive.org/details/@'..user..'?tab=uploads&page='
 	repeat
-		print("Page ", page)
 		local rc, headers, content = http.request(user_url..tostring(page))
 		if rc ~= 0 then
 			print("Error: "..http.error(rc), rc)
@@ -32,58 +39,78 @@ function archive_user_uploads(user)
 			done = true
 		end
 		for link, title in content:gmatch('\n            <a href%="/details/(.-)" title="(.-)"') do
-			print(link, title)
+			-- print(link, title)
 			res[#res + 1] = link
 		end
+		print("Page "..page..": "..tostring(#res).."data")
 		page = page + 1
+		
 	until done
 	return res
 end
 
-if #arg == 1 then
-	os.execute('del romset.htm')
-	rc, headers = http.request{url = arg[1], output_filename = 'romset.htm'}
+-- user_url=https://archive.org/download/nes-roms
+-- user_url=https://archive.org/details/nes-roms
+-- user_url=nes-roms
+function archive_generate_db(user_url, category)
+	local fo, output_fname, rc, headers, content, nn
+	if user_url == "" or user_url == nil then
+		return false
+	end
+	
+	category = category or "general"
+	if user_url:match('/details/') then
+		user_url = user_url:gsub('/details/','/download/')
+		output_fname = user_url:match('download/(.-)$')
+	elseif user_url:match('/download/') then
+		output_fname = user_url:match('download/(.-)$')
+	else
+		output_fname = user_url
+		user_url = 'https://archive.org/download/'..user_url
+	end
+	output_fname = output_fname:gsub("%W","_")..".csv"
+	
+	fo = io.open(output_fname, "w")
+	if fo == nil then
+		print('Error open a file '..output_fname)
+		return false
+	end
+	rc, headers, content = http.request(user_url)
 	if rc ~= 0 then
 		print("Error: "..http.error(rc), rc)
 		return false
 	end
-	print("#url="..url)
-	content = load_file('romset.htm')
+	
+	fo:write("#category="..category.."\n")
+	fo:write("#url="..user_url.."\n")
+	nn = 0
 	for w1,w2,w3 in content:gmatch('<td><a href="(.-)">(.-)</a>.-</td>.-<td>.-</td>.-<td>(.-)</td>.-</tr>') do
 		if w1:match("%.jpg$") or w1:match("%.torrent$") or w1:match("%.xml$") or w1:match("%.sqlite$") 
 		or w1:match("^history/") or w1:match("^/details/") 
 		then
-			--print("Ignore: ", w1)
+			print("Ignore: ", w1)
 		else
 			w2 = w2:gsub("&amp;", "&")
 			w2 = w2:gsub("%.%w+$", "")
-			print(w1.."|"..w2.."|"..w3)
+			fo:write(w1.."|"..w2.."|"..w3.."\n")
+			nn = nn + 1
 		end
 	end
+	fo:close()
+	print("Successfully process "..nn.." data")
+	return true
+end
+
+if #arg == 1 then
+	archive_generate_db(arg[1])
 else
-	local list_uploads
 	print(string.format("Generate ROM database from archieve.org.\nUsage: \n\t#> %s [url]", arg[0]))
 	print(string.format("\t#> %s \"https://archive.org/download/nes-roms\" ", arg[0]))
 	
-	-- rc, headers = http.request{url = 'https://archive.org/details/@aitus95?tab=uploads', output_filename = 'list_user_uploads1.htm'}
-	-- if rc ~= 0 then
-		-- print("Error: "..http.error(rc), rc)
-		-- return false
-	-- end
-	-- rc, headers = http.request{url = 'https://archive.org/details/@aitus95?tab=uploads&page=2', output_filename = 'list_user_uploads2.htm'}
-	-- if rc ~= 0 then
-		-- print("Error: "..http.error(rc), rc)
-		-- return false
-	-- end
-	-- rc, headers = http.request{url = 'https://archive.org/details/@aitus95?tab=uploads&page=5', output_filename = 'list_user_uploads5.htm'}
-	-- if rc ~= 0 then
-		-- print("Error: "..http.error(rc), rc)
-		-- return false
-	-- end
-	-- print(headers)
+	local list_uploads
 	list_uploads = archive_user_uploads('aitus95')
 	for i,v in pairs(list_uploads) do
-		print(i,v)
+		print(i,"https://archive.org/download/"..v)
 	end
 	return true
 end
