806b1a2e625a7a3526849cab332a2c9051e00350
Leon Kasovan <leonkasovan@gmail.com>
2023-08-19 19:30:50 +0700

    fix clone.lua 	modified:   lua-script/github/clone.lua

--- lua-script/github/clone.lua
+++ lua-script/github/clone.lua
@@ -14,11 +14,21 @@ function download_directory(path)
 	value = json.decode(content)
 	for i,v in pairs(value.payload.tree.items) do
 		if v.contentType == "directory" then
-			print("\nNew directory "..v.name)
-			lfs.mkdir(v.name)
-			lfs.chdir(v.name)
-			download_directory(path..'/'..v.name)
-			lfs.chdir("..")
+			local pos = v.name:find('/',1,true)
+			if pos == nil then
+				print("\nNew directory "..v.name)
+				lfs.mkdir(v.name)
+				lfs.chdir(v.name)
+				download_directory(path..'/'..v.name)
+				lfs.chdir("..")
+			else
+				local dirname = v.name:sub(1, pos - 1)
+				print("\nNew (empty) directory "..dirname)
+				lfs.mkdir(dirname)
+				lfs.chdir(dirname)
+				download_directory(path..'/'..dirname)
+				lfs.chdir("..")
+			end
 		else --v.contentType == "file"
 			print("Download file "..v.name)
 			rc, headers = http.request{url = path..'/'..v.name, output_filename = v.name}
@@ -30,4 +40,10 @@ function download_directory(path)
 	return true
 end
 
-download_directory('https://github.com/batocera-linux/batocera.linux/tree/master/board/batocera/rockchip/rk3399')
+if #arg == 0 then
+	print("Clone specific directory from github.com.\nUsage:\n\t#>luaxx %s https://github.com/batocera-linux/batocera.linux/tree/master/board/batocera/rockchip/rk3399", arg[0])
+	download_directory('https://github.com/batocera-linux/batocera.linux/tree/master/board/batocera/rockchip/rk3399')
+elseif #arg == 1 then
+	download_directory(arg[1])
+end
+
